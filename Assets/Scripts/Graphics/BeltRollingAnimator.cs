60393663470c940c5b6234b61a8abcf043881170
Vilem Otte <vilem.otte@windowslive.com>
2016-12-10 17:00:22 +0100

    Initial graphics commit

---
+++ Assets/Scripts/Graphics/BeltRollingAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltRollingAnimator : MonoBehaviour
+{
+    private Renderer rend;
+    private float offset = 0.0f;
+    public static float offsetSpeed = 0.01f;
+
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+	}
+	
+	void FixedUpdate ()
+    {
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0.0f));
+        offset -= offsetSpeed;
+    }
+}
