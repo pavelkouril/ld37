0c531dfa745c7dbf0122042334566368f23b1ed9
Vilem Otte <vilem.otte@windowslive.com>
2016-12-10 19:43:04 +0100

    Graphics update - hands

---
+++ Assets/Scripts/Hand/Hand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public Transform core;
+    public Transform lowerHand;
+    public Transform upperHand;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
