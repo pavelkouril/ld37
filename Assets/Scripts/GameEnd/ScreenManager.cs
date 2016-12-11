e7528dbd4ce96466be5f6f49dc5b8c16b55ed1c5
Pavel Kouřil <pk@pavelkouril.cz>
2016-12-12 00:36:19 +0100

    added game end scene and high scores saving

---
+++ Assets/Scripts/GameEnd/ScreenManager.cs
@@ -0,0 +1,30 @@
+﻿using OneRoomFactory.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OneRoomFactory.GameEnd
+{
+    public class ScreenManager : MonoBehaviour
+    {
+        public Text HighScoreTable;
+
+        private HighScoresManager hsm;
+
+        private void Awake()
+        {
+            hsm = GetComponent<HighScoresManager>();
+        }
+
+        private void Start()
+        {
+            var str = string.Empty;
+            foreach (var p in hsm.GetTop10Scores())
+            {
+                str += p.Key.ToString("yyyy-MM-dd HH:mm") + "          " + p.Value + "\n";
+            }
+            HighScoreTable.text = str;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/HighScoresManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace OneRoomFactory.Managers
+{
+    public class HighScoresManager : MonoBehaviour
+    {
+        const string Filename = "hs.txt";
+
+        public void WriteHighScore(int score)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Filename, true))
+            {
+                file.WriteLine(DateTime.Now.ToBinary() + " " + score);
+            }
+        }
+
+        public IEnumerable<KeyValuePair<DateTime, int>> GetTop10Scores()
+        {
+            var dict = new Dictionary<DateTime, int>();
+            if (System.IO.File.Exists(Filename))
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(Filename))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var split = line.Split(' ');
+                        var date = DateTime.FromBinary(long.Parse(split[0]));
+                        var score = int.Parse(split[1]);
+                        dict.Add(date, score);
+                    }
+                }
+            }
+            return dict.OrderByDescending(p => p.Value).Take(10);
+        }
+    }
+}
