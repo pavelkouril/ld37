346f2195656322b8ebbe7386efc72aaa13cf61c0
Vilém Otte <Vilm Otte>
2016-12-12 02:17:42 +0100

    Scene updated

---
+++ Assets/Scripts/CubemapGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CubemapGenerator : ScriptableWizard
+{
+    public Transform renderPosition;
+    public Cubemap cubemap;
+
+    void OnWizardUpdate()
+    {
+        helpString = "Select transform to render" +
+            " from and cubemap to render into";
+        if (renderPosition != null && cubemap != null)
+        {
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+        }
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubeCam", typeof(Camera));
+
+        go.transform.position = renderPosition.position;
+        go.transform.rotation = Quaternion.identity;
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("Zgragselus/Render Cubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard("Render CubeMap", typeof(CubemapGenerator), "Render!");
+    }
+}
